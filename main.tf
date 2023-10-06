f584f8981cbb22773fa5ba2c02e5ca817213827e
JohnnyDoer <bajiraoayush@gmail.com>
2023-10-06 16:15:44 -0700

    added ownership controls

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 resource "aws_s3_bucket" "s3-website-bucket" {
   bucket = var.s3-website-bucket-name # Specify a unique bucket name
 }
+
+resource "aws_s3_bucket_ownership_controls" "s3-website-bucket" {
+  bucket = aws_s3_bucket.s3-website-bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
