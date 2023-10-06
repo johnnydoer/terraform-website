c0cb9b577e91951c893791b1216ad621992a645a
JohnnyDoer <bajiraoayush@gmail.com>
2023-10-06 16:16:50 -0700

    made it publically accessible

--- main.tf
+++ main.tf
@@ -10,3 +10,12 @@ resource "aws_s3_bucket_ownership_controls" "s3-website-bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "s3-website-bucket" {
+  bucket = aws_s3_bucket.s3-website-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
