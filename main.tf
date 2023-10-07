3abd573841ced4e7017c197585017ce13a45f7ed
JohnnyDoer <bajiraoayush@gmail.com>
2023-10-06 17:33:11 -0700

    added website and uploading complete folder to s3

--- main.tf
+++ main.tf
@@ -28,3 +28,27 @@ resource "aws_s3_bucket_acl" "s3-website-bucket" {
   bucket = aws_s3_bucket.s3-website-bucket.id
   acl    = "public-read"
 }
+
+module "template_files" {
+  source = "hashicorp/dir/template"
+
+  base_dir = "${path.module}/awesome-portfolio-websites"
+}
+
+resource "aws_s3_object" "portfolio" {
+  for_each = module.template_files.files
+
+  bucket = aws_s3_bucket.s3-website-bucket.id
+  key          = each.key
+  content_type = each.value.content_type
+
+  # The template_files module guarantees that only one of these two attributes
+  # will be set for each file, depending on whether it is an in-memory template
+  # rendering result or a static file on disk.
+  source  = each.value.source_path
+  content = each.value.content
+
+  # Unless the bucket has encryption enabled, the ETag of each object is an
+  # MD5 hash of that object.
+ 
+}
